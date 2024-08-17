0554402e4f8af18efd7c01a61a02143e9159d31b
GuadalupeSaavedra <230349@utxicotepec.edu.mx>
2024-08-16 23:18:39 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// console.log("Hola Mundo! desde la cosola");
+var app = document.getElementById('app');
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+typewriter.typeString('Desarrolladora Frontend Jr. | Diseñadora')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¡Si lo puedes imaginar, lo puedes crear!')
+    .pauseFor(2500)
+    .deleteChars(6)
+    .typeString('<strong>programar!<strong>')
+    .pauseFor(2500)
+    .start();
